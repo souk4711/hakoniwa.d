e0a60ef026540ab21b541d7d3dcb500c167c1c19
John Doe <johndoe@example.com>
2025-05-26 01:31:16 +0800

    fix: Make xdg-open work properly when using D-Bus

--- stdlib.sh
+++ stdlib.sh
@@ -7,7 +7,7 @@ hakoniwa_run() {
   shift; shift
 
   # the command is already running in a hakoniwa.d container, donot create a nested container
-  if [ -n "$HAKONIWAD_CONTAINER" ]; then
+  if [ -d "/run/hakoniwad" ]; then
     exec "$bin" "$@"
     return 0
   fi
